09e5afc38aca3fb4650233b4dddc77f6989bd77d
aakashgujar <aakashgujar2001@gmail.com>
2025-01-17 23:59:48 +0530

    minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ export default function Page() {
               <Link href="#" className="text-white hover:underline">
                 Discord
               </Link>
-              . I also manage a thriving community of over 1,000 members called{" "}
+              . I also manage a thriving community of over 1,000+ members called{" "}
               <Link href="#" className="text-white hover:underline">
                 43labs
               </Link>
